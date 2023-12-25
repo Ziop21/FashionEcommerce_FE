3fe0cbaccccacc8ee15bb60b24385b2612f79b4e
npp2107 <142470779+npp2107@users.noreply.github.com>
2023-12-26 00:39:10 +0700

    error: cookies 12

--- src/config/ApplicationConfig.ts
+++ src/config/ApplicationConfig.ts
@@ -1,5 +1,5 @@
 // export const API_BACKEND_URL = 'http://localhost:8081';
-export const API_BACKEND_URL = '192.168.1.121';
+export const API_BACKEND_URL = 'http://192.168.1.121:8081';
 
 export const JWT_SECRET_KEY = '======================FashionEcommerce=Spring===========================';
 
