754ac6811b055e3adf70a6ac12b0d3af40a8aa4f
npp2107 <142470779+npp2107@users.noreply.github.com>
2023-12-27 01:56:26 +0700

    fix bugs to deploy

--- src/app/forgot-password/verifyEmail/page.tsx
+++ src/app/forgot-password/verifyEmail/page.tsx
@@ -9,14 +9,17 @@ const Verify = (token: any) => {
     useEffect(() => {
         const getEmailFromToken = async () => {
             const email = await findEmail(token.searchParams.token);
-            if (email){
-                localStorage.setItem('FE_UserEmail', email);
-                localStorage.setItem('FE_Token', token.searchParams.token);
+            if (email) {
+                await new Promise<void>(resolve => {
+                    localStorage.setItem('FE_UserEmail', email);
+                    localStorage.setItem('FE_Token', token.searchParams.token);
+                    resolve();
+                });
             }
         }
         getEmailFromToken();
         router.push('/forgot-password/infor')
     }, [token])
 }
- 
+
 export default Verify;
