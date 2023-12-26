0ba5e3c0a198ffe1620e961f207f1e980f780684
npp2107 <142470779+npp2107@users.noreply.github.com>
2023-12-27 02:38:44 +0700

    fix bugs to deploy

--- src/app/register/verifyEmail/page.tsx
+++ src/app/register/verifyEmail/page.tsx
@@ -2,39 +2,44 @@
 
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { useSearchParams } from 'next/navigation'
 
 import verifyEmailToken from "@/pages/api/guest/auth/register/verifyEmailToken";
 import toast from "react-hot-toast";
 
-interface VerifyProps {
-    searchParams: {
-        token: string;
-    }
-}
+// interface VerifyProps {
+//     searchParams: {
+//         token: string;
+//     }
+// }
 
-const Verify = (token: VerifyProps) => {
+const Verify = () => {
     const router = useRouter();
+    const searchParams = useSearchParams()
 
     useEffect(() => {
-        if (token) {
-            const verifyToken = async () => {
-                // console.log("token");
-                // console.log(token.searchParams.token);
-                try {
-                    const response = await verifyEmailToken(token.searchParams.token);
-                    if (response) {
-                        localStorage.setItem('FE_UserEmail', response);
-                        toast.success("Verify successfully...")
-                        router.push("/register/infor")
+        if (searchParams) {
+            const token = searchParams.get('token')
+            if (token){
+                const verifyToken = async () => {
+                    console.log("token");
+                    console.log(token);
+                    try {
+                        const response = await verifyEmailToken(token);
+                        if (response) {
+                            localStorage.setItem('FE_UserEmail', response);
+                            toast.success("Verify successfully...")
+                            router.push("/register/infor")
+                        }
+                    } catch (error) {
+                        throw error;
                     }
-                } catch (error) {
-                    throw error;
+    
                 }
-              
+                verifyToken();
             }
-            verifyToken();
         }
-    }, [token])
+    }, [searchParams])
 
 
 }
